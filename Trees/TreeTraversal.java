cb4dddb2e85f7fd1e0b63dda2aa417869f556344
Madhava-sb <228x1a05d2@khitguntur.ac.in>
2026-03-09 23:37:49 +0530

    day 10 : added a method to check whether 2 trees are identical or not

--- Trees/TreeTraversal.java
+++ Trees/TreeTraversal.java
@@ -113,8 +113,8 @@ class TreeTraversal{
     }
 
     public static int noOfLeafNodes(Node root) {
-        int left = Integer.MIN_VALUE;
-        int right = Integer.MIN_VALUE;
+        int left = 0;
+        int right = 0;
 
         if(root == null) return 0;
 
@@ -125,6 +125,14 @@ class TreeTraversal{
         }
 
         return left + right;
+    }
+
+    public static boolean isSame(Node root1, Node root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.data != root2.data) return false;
+
+        return isSame(root1.left, root2.left) && isSame(root1.right, root2.right);
     }
 
     public static void main(String[] args) {
